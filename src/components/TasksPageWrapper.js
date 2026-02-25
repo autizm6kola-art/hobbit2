3320512791cd610336a2c9727502bd50b1320cf8
Светлана <autizm6kola@yandex.com>
2026-02-25 07:17:29 +0300

    50

--- src/components/TasksPageWrapper.js
+++ src/components/TasksPageWrapper.js
@@ -38,7 +38,7 @@ function TasksPageWrapper({
   setSelectedRange,
   goBack
 }) {
-  const rangeSize = 100;
+  const rangeSize = 50;
 
   // 1️⃣ Создаём все диапазоны, как в меню
   const ranges = useMemo(() => {
